eab6a0743b90ccb0f6deb028d31103a824680c16
mdh960 <harringtonm@gilmour.org>
2017-02-17 11:18:26 -0500

    Pigo servo, student dance1

--- student.py
+++ student.py
@@ -63,8 +63,9 @@ class GoPiggy(pigo.Pigo):
     def dance(self):
         print("Piggy dance")
         ##### WRITE YOUR FIRST PROJECT HERE
-        # 18=half
-        # 36=full
+        # 7=quarter
+        # 15=half
+        # 30=full
         self.shimmy()
         self.chacha()
         #self.sprinkler()
@@ -81,7 +82,7 @@ class GoPiggy(pigo.Pigo):
     def chacha(self):
         print('chacha')
         for x in range (1):
-            self.encR(32)
+            self.encR(30)
             self.encL(4)
             self.encF(15)
             time.sleep(.25)
