1f5ae0f9594fe1b82c0f271d3054cc06c032f9e2
mdh960 <harringtonm@gilmour.org>
2017-02-17 11:32:29 -0500

    Pigo servo, student dance1

--- student.py
+++ student.py
@@ -104,7 +104,7 @@ class GoPiggy(pigo.Pigo):
             self.encR(4)
             self.encL(8)
             self.encR(4)
-            self.encR(32)
+            self.encR(30)
             self.encB(15)
             time.sleep(.25)
             self.encB(15)
