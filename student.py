6cbf47714a9fb27b5a5c2726eea3c7f416234de6
mdh960 <harringtonm@gilmour.org>
2017-02-22 11:24:38 -0500

    Pigo servo, student dance1

--- student.py
+++ student.py
@@ -51,7 +51,7 @@ class GoPiggy(pigo.Pigo):
                 "w": ("Sweep", self.sweep),
                 "s": ("Check status", self.status),
                 "q": ("Quit", quit)
-
+                }
         # loop and print the menu...
         for key in sorted(menu.keys()):
             print(key + ":" + menu[key][0])
