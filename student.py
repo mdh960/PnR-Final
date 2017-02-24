7f389dfb14ad76c6aa88507a4865a2437a9b07ff
mdh960 <harringtonm@gilmour.org>
2017-02-24 11:15:19 -0500

    Pigo servo, student dance1

--- student.py
+++ student.py
@@ -94,7 +94,8 @@ class GoPiggy(pigo.Pigo):
     def restore_heading(self):
         print("returning to starting position")
         if self.turn_track > 0:
-        elif self.turn_track < 0:
+
+        #elif self.turn_track < 0:
 
 
 
