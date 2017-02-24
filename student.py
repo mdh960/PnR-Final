4f98ee2e25a54a08e5283c8dfa6252a911e01712
mdh960 <harringtonm@gilmour.org>
2017-02-24 11:10:30 -0500

    Pigo servo, student dance1

--- student.py
+++ student.py
@@ -93,7 +93,7 @@ class GoPiggy(pigo.Pigo):
 
     def restore_heading(self):
         print("returning to starting position")
-        val = int(raw_input('/nBy how much?: '))
+        val = input('/nBy how much?: ')
         if self.encR(val):
             self.encL(val)
 
