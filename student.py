c1e49dfb138e2777e6e6e0ecd340874d7d19471e
mdh960 <harringtonm@gilmour.org>
2017-04-21 11:27:28 -0400

    restoring choose path to normal

--- student.py
+++ student.py
@@ -70,7 +70,7 @@ class GoPiggy(pigo.Pigo):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         # this is the loop part of the "main logic loop"
         while True:
-            answer = self.smart_scanR()
+            answer = self.choose_path()
             if answer:
                 self.cruise()
             else:
