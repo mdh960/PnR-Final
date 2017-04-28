9d7236a2be1431fcb8659b9fcfb32245ce800f3b
mdh960 <harringtonm@gilmour.org>
2017-04-28 10:17:29 -0400

    Testing half scan problem

--- pigo.py
+++ pigo.py
@@ -208,7 +208,6 @@ class Pigo(object):
             stop()
         servo(self.MIDPOINT)
         time.sleep(0.05)
-        disable_servo()
 
     def calibrate(self):
         print("Calibrating...")
